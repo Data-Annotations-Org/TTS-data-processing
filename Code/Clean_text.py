a15e6fa69f7130216a6055bb163b1881c8bee1c2
tzmadela <thandinkosi.madela@yahoo.com>
2024-02-28 13:05:00 +0200

    Tswana

--- Code/Clean_text.py
+++ Code/Clean_text.py
@@ -40,7 +40,7 @@ def clean_text(input_file, output_dir):
 
     print("Files created successfully!")
 
-input_file = r"C:\Users\thand\OneDrive\Documents\Botlale Ai\Data_Processing\TTS-data-processing\Scrapped_data\Seipo_Pedi.txt"
-output_directory = r"C:\Users\thand\OneDrive\Documents\Botlale Ai\Data_Processing\TTS-data-processing\Clean_data\SePedi"
+input_file = r"C:\Users\thand\OneDrive\Documents\Botlale Ai\Data_Processing\TTS-data-processing\Scrapped_data\Tswana.txt"
+output_directory = r"C:\Users\thand\OneDrive\Documents\Botlale Ai\Data_Processing\TTS-data-processing\Clean_data\SeTswana"
 
 clean_text(input_file, output_directory)
